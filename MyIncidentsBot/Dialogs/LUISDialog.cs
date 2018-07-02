56433def713af4dec9b480439d12fb6e268dbe25
Simona Tsenova <simona.tsenova@telelink.com>
2018-07-02 13:51:08 +0300

    Add incident id prompt when trying to get incident state

--- MyIncidentsBot/Dialogs/LUISDialog.cs
+++ MyIncidentsBot/Dialogs/LUISDialog.cs
@@ -7,6 +7,7 @@ using MyIncidentsBot.Models.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MyIncidentsBot.Dialogs
@@ -15,6 +16,12 @@ namespace MyIncidentsBot.Dialogs
     [Serializable]
     public class LUISDialog : LuisDialog<object>
     {
+        private readonly IList<Incident> incidents = new List<Incident>()
+        {
+            new Incident() { ID = "INC0019503", Urgency = UrgencyType.Medium, Description = "Node is down", State = "In progress" },
+            new Incident() { ID = "INC0019504", Urgency = UrgencyType.Low, Description = "Button disabled", State = "Closed" }
+        };
+
         [LuisIntent("None")]
         public async Task None(IDialogContext context, LuisResult result)
         {
@@ -42,15 +49,9 @@ namespace MyIncidentsBot.Dialogs
         [LuisIntent("GetAllIncidents")]
         public async Task GetAllIncidents(IDialogContext context, LuisResult result)
         {
-            var incidents = new List<Incident>
-            {
-                new Incident() { ID = "INC0019503", Urgency = UrgencyType.Medium, Description = "Node is down" },
-                new Incident() { ID = "INC0019504", Urgency = UrgencyType.Low, Description = "Button disabled" }
-            };
-
             await context.PostAsync("Here are your incidents:");
             string incidentsReply = string.Empty;
-            var incidentsCount = incidents.Count;
+            var incidentsCount = this.incidents.Count;
             for (int i = 0; i < incidentsCount; i++)
             {
                 incidentsReply += $"{i}: INCIDENT: {incidents[i].Description}, URGENCY: {incidents[i].Urgency} \n";
@@ -63,37 +64,36 @@ namespace MyIncidentsBot.Dialogs
         [LuisIntent("GetIncidentState")]
         public async Task GetIncident(IDialogContext context, LuisResult result)
         {
-            var incidents = new List<Incident>
-            {
-                new Incident() { ID = "INC0019503", Urgency = UrgencyType.Medium, Description = "Node is down", State = "In progress" },
-                new Incident() { ID = "INC0019504", Urgency = UrgencyType.Low, Description = "Button disabled", State = "Closed" }
-            };
-
             var incidentId = string.Empty;
             if (result.Entities.Count > 0)
             {
-                incidentId = result.Entities.FirstOrDefault(e => e.Type == "ID").Entity;
+                incidentId = result.Entities.FirstOrDefault(e => e.Type == "IncidentID").Entity;
             }
 
             if (string.IsNullOrEmpty(incidentId))
             {
-                await context.PostAsync("What's the ID of the incident you want me to check?");
+                PromptDialog.Text(
+                    context: context, 
+                    resume: OnIncidentIdPromptComplete, 
+                    prompt: "What's the ID of the incident you want me to check?", 
+                    retry: "Sorry, I didn't understant that. Please try again.");
             }
             else
             {
-                var incident = incidents.Where(i => i.ID.ToLower() == incidentId.ToLower()).FirstOrDefault();
+                // Get incident
+                var incident = this.incidents.Where(i => i.ID.ToLower() == incidentId.ToLower()).FirstOrDefault();
 
                 if (incident == null)
                 {
-                    await context.PostAsync($"No incident with this Id {incidentId} has been found.");
+                    await context.PostAsync($"No incident with Id {incidentId} has been found.");
                 }
                 else
                 {
-                    await context.PostAsync($"Incident {incidentId} is {incident.State}.");
+                    await context.PostAsync($"Incident with Id {incidentId} is {incident.State}.");
                 }
             }
 
-            context.Wait(MessageReceived);
+            //context.Wait(MessageReceived);
         }
 
         private async Task OnCreateIncidentComplete(IDialogContext context, IAwaitable<Incident> result)
@@ -102,6 +102,36 @@ namespace MyIncidentsBot.Dialogs
             var incident = await result;
 
             await context.PostAsync("Successfully created an incident. Stay tuned for updates on your incident.");
+            context.Wait(MessageReceived);
+        }
+
+        private async Task OnIncidentIdPromptComplete(IDialogContext context, IAwaitable<string> result)
+        {
+            var incidentId = await result;
+            var pattern = @"inc[0-9]{7}";
+            var regex = new Regex(pattern, RegexOptions.IgnoreCase);
+            var match = regex.Match(incidentId);
+            if (match.Success)
+            {
+                incidentId = match.Value;
+
+                // Get incident
+                var incident = this.incidents.Where(i => i.ID.ToLower() == incidentId.ToLower()).FirstOrDefault();
+
+                if (incident == null)
+                {
+                    await context.PostAsync($"No incident with Id {incidentId} has been found.");
+                }
+                else
+                {
+                    await context.PostAsync($"Incident with Id {incidentId} is {incident.State}.");
+                }
+            }
+            else
+            {
+                await context.PostAsync("I'm sorry but you should provide valid incident ID.");
+            }
+            
             context.Wait(MessageReceived);
         }
     }
